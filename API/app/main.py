3b6ab47543164f6e766dd12551c73c24c08fc105
Arif Haidari <arifhaidari336@gmail.com>
2024-08-23 13:39:17 +0200

    update docker compose

--- API/app/main.py
+++ API/app/main.py
@@ -13,22 +13,8 @@ from .auth.auth_utils import get_current_user
 app = FastAPI()
 
 
-# You need to create the tables in your PostgreSQL database. 
-# You can do this using SQLAlchemy directly or with Alembic for migrations.
-# Option 1: Directly with SQLAlchemy
-# Add the following to main.py:
-
 from .database.db import Base, engine
 Base.metadata.create_all(bind=engine)
-
-# Using Alembic:
-# Steps:
-# Initialize Alembic: Run alembic init alembic to set up Alembic in your project.
-# Create Migrations: When you make changes to your SQLAlchemy models, 
-# you create a new migration script with alembic revision --autogenerate -m "Description of change".
-# Apply Migrations: Run alembic upgrade head to apply the migration to your database.
-# Manage Versions: Alembic keeps track of schema versions, allowing you to upgrade or downgrade the 
-# database schema as needed.
 
 
 # Include routers for different functionalities
@@ -45,6 +31,3 @@ app.include_router(retrain_router, tags=["Retrain"])
 def read_root():
      return {"message": "Welcome to the Plant Detection API!"}
 
-# @app.get("/test-api")
-# def read_root(current_user: str = Depends(get_current_user)):
-#      return {"message": "The API is up and running with authorization"}
